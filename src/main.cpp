862eeb00aae5d1ee8a7d1f9bdeb85565d4fc6a3f
Phillip C <phizzajc@gmail.com>
2024-06-23 00:16:34 +0800

    add xcode target

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//
+// C++20 CLI Template
+// (c) 2024 Moon Happy - Phillip Cooper.
+//
+// This code is licensed under MIT license (see LICENSE for details)
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+}
